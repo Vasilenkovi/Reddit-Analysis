3810e3fa63a71d41c5ed2de8496a3f12ca32d255
CatGamer7 <99312965+CatGamer7@users.noreply.github.com>
2024-03-12 02:37:16 +0700

    (RED-15) add: active user extraction feature

--- reddit_parser.py
+++ reddit_parser.py
@@ -1,5 +1,5 @@
 from omegaconf import DictConfig
-from ORM import ORM_submission, ORM_comment
+from ORM import ORM_submission, ORM_comment, ORM_subreddit, ORM_subreddit_active_users
 import hydra
 from praw import Reddit
 import praw.reddit
@@ -62,7 +62,7 @@ class Extended_Reddit_RO(Reddit):
         elif isinstance(submission, praw.reddit.Submission):
             submission_url = submission.url #Network call
         else:
-            raise AttributeError("submission must be str (assumed url) or praw.reddit.Submission")
+            raise ValueError("submission must be str (assumed url) or praw.reddit.Submission")
         
         submission_name = submission.name
         submission_title = submission.title
@@ -97,6 +97,49 @@ class Extended_Reddit_RO(Reddit):
 
         return submission_instance, comment_generator()
     
+    def parse_subreddit_users(self, subreddit_display_name: str, sort_discipline: str = "hot", limit: int = 1000, comment_replace_limit: int | None = 0, comment_replace_threshold: int = 1) -> tuple[ORM_subreddit, Generator[list[ORM_subreddit_active_users], None, None]]:
+        """Description: Parses active users of specified subreddit. Users are selected among posters and commentors. Active users are defined by sort_discipline. Submissions from deleted accounts are dropped. Method uses 1 network call for every 100 submissions (~1 sec for 100 submissions) + network call for every comment replacement.\n
+        Parameters: \n
+            subreddit_display_name - visible, case insensitive name of subreddit to search from;\n
+            sort_discipline - how active users are defined. Options: "hot", "new", "top". Raises ValueError otherwise;\n
+            limit - the maximum number of submissions to retrieve. Reddit API has been known to return not more than 1000 submissions;\n
+            comment_replace_limit - how many "more comments" to replace in each submission. Each replacement incurs a network call (around 1 second of wait time);\n
+            comment_replace_threshold - how many additional replies "more comments" must have to incur a replacement call;\n
+        Returns: Tuple of Subreddit and generator of Subreddit's active users grouped by submissions"""
+
+        subreddit = self.subreddit(subreddit_display_name)
+        sub_orm = ORM_subreddit(subreddit.fullname, subreddit_display_name, subreddit.url)
+
+        submission_generator = None
+        match sort_discipline:
+            case "hot":
+                submission_generator = subreddit.hot(limit = limit)
+            case "new":
+                submission_generator = subreddit.new(limit = limit)
+            case "top":
+                submission_generator = subreddit.top(time_filter = "month", limit = limit)
+            case _:
+                raise ValueError('sort_discipline must be a str in ("hot", "new", "top")')
+
+        def author_generator() -> Generator[ORM_subreddit_active_users, None, None]:
+            """Helper function to create a comment author generator"""
+
+            for submission in submission_generator:
+                total_user_list = []
+
+                if submission.author: #Submissions from deleted accounts set this field to None
+                    total_user_list.append(ORM_subreddit_active_users(subreddit.fullname, submission.author_fullname))
+
+                submission.comments.replace_more(limit = comment_replace_limit, threshold = comment_replace_threshold)
+
+                for comment in submission.comments.list():
+                    if comment.author: #Comments from deleted accounts set this field to None
+                        total_user_list.append(ORM_subreddit_active_users(subreddit.fullname, comment.author_fullname))
+
+                yield total_user_list
+
+        return sub_orm, author_generator()
+ 
 
 def parse_subreddit_to_db(reddit_config: DictConfig, subreddit_config: DictConfig, db_config: DictConfig) -> None:
     """Description: Parses subreddit and writes results to database based on dictionaries.\n
@@ -179,6 +222,48 @@ def parse_submission_to_db(reddit_config: DictConfig, submission_config: DictCon
     for com in com_gen:
         com.write_to_MySQL(db_config)
 
+def parse_subreddit_users_to_db(reddit_config: DictConfig, subreddit_users_config: DictConfig, db_config: DictConfig) -> None:
+    """Description: Parses subreddit for active users and writes results to database based on dictionaries.\n
+    Keys accessed:\n
+        reddit_config.client_id - client id of registered Reddit app;\n
+        reddit_config.client_secret - client secret of registered Reddit app;\n
+        reddit_config.user_agent - unique user agent;\n
+        \n
+        subreddit_users_config.subreddit_display_name - user-visible subreddit name to search for;\n
+        subreddit_users_config.sort_discipline - how active users are defined. Options: "hot", "new", "top". Raises ValueError otherwise;\n
+        subreddit_users_config.limit - the maximum number of submissions to retrieve. Reddit API has been known to return not more than 1000 submissions;\n
+        subreddit_users_config.comment_replace_limit - how many "more comments" to replace in each submission. Each replacement incurs a network call (around 1 second of wait time);\n
+        subreddit_users_config.comment_replace_threshold - how many additional replies "more comments" must have to incur a replacement call;\n
+        \n
+        db_config.user - database user with dml privileges to provided database;\n
+        db_config.password - password for provided user;\n
+        db_config.database - database with necessary tables;\n
+        db_config.host - address for database connection;\n
+        db_config.port - port for database connection;\n
+        db_config.use_pure - boolean to use pure python connection instead of c implementation. True value recommended;\n
+    Returns: None"""
+
+    reddit = Extended_Reddit_RO(
+        client_id = reddit_config.client_id,
+        client_secret = reddit_config.client_secret,
+        user_agent = reddit_config.user_agent
+    )
+
+    sub, submission_users = reddit.parse_subreddit_users(
+        subreddit_display_name = subreddit_users_config.subreddit_display_name,
+        sort_discipline = subreddit_users_config.discipline,
+        limit = subreddit_users_config.limit,
+        comment_replace_limit = subreddit_users_config.comment_replace_limit,
+        comment_replace_threshold = subreddit_users_config.comment_replace_threshold
+    )
+
+    sub.write_to_MySQL(db_config)
+
+    for users in submission_users:
+        for user in users:
+            user.write_to_MySQL(db_config)
+
+
 @hydra.main(version_base = None, config_path = "conf", config_name = "conf.yaml")
 def main(cfg : DictConfig) -> None:
     """Main for testing and debugging"""
@@ -188,6 +273,9 @@ def main(cfg : DictConfig) -> None:
     
     if (cfg.mock.get("submission_query")):
         parse_submission_to_db(cfg.client, cfg.mock.submission_query, cfg.db)
+
+    if (cfg.mock.get("subreddit_users_query")):
+        parse_subreddit_users_to_db(cfg.client, cfg.mock.subreddit_users_query, cfg.db)
 
 if __name__ == "__main__":
     main()
