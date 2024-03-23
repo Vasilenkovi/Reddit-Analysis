153b60329db1c5a5a6c26643feb9f50767452a63
theSaladin <99478272+No1CareZ@users.noreply.github.com>
2024-03-23 21:18:40 +0700

    asgi.py

--- WebRed/DjangoRed/asgi.py
+++ WebRed/DjangoRed/asgi.py
@@ -7,10 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
-import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter
+from channels.routing import URLRouter
 
 from django.core.asgi import get_asgi_application
+from django.urls import path
+
+from WebRedApp.consumer import YourConsumer
+
+import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoRed.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter([
+            path('ws', YourConsumer.as_asgi())
+        ])
+    )
+})
