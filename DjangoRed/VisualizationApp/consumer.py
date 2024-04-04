30635b74c1a78af3e800ac752f042d7878b94762
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2024-04-04 22:53:11 +0700

    CHANGED: consumer.py

--- DjangoRed/VisualizationApp/consumer.py
+++ DjangoRed/VisualizationApp/consumer.py
@@ -6,7 +6,7 @@ class ClusterConsumer(WebsocketConsumer):
         self.accept()
         self.send(text_data=json.dumps({
                 'type':'connection_established',
-             'message': "Connected!"
+             'message': "Connected!!"
             }
         ))
     def receive(self, text_data=None, bytes_data=None):
