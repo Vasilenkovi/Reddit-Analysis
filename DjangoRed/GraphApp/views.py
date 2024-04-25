60522f67f1dd8b46bf923435c473f29cf1961b37
CatGamer7 <99312965+CatGamer7@users.noreply.github.com>
2024-04-25 22:14:09 +0700

    (MISC) uodate: pickle5 + missing dependecies

--- DjangoRed/GraphApp/views.py
+++ DjangoRed/GraphApp/views.py
@@ -7,40 +7,37 @@ from DatasetViewApp.forms import Dataset_operation_form
 
 # Create your views here.
 def base_graph_view(request):  
-    #context = {
-    #    "dataset_ids": [],
-    #    "data": [],
-    #    "error": None,
-    #    "downloadable": None
-    #}
-#
-    #if request.method == "GET":
-    #    return render()
-#
-    #form = Dataset_operation_form(request)
-    #dataset_ids = form.get_ids_as_list()
-#
-    #if not dataset_ids:
-    #    return render(request, "", context = context)
-#
-    #if not form.is_valid():
-    #    context["error"] = "Unsupported dataset combination"
-    #    context["dataset_ids"] = dataset_ids
-#
-    #    return render(request, "", context = context)
-#
-    #column_list = ["su.user_full_name", "s.display_name"]
-    #user_data = sub_select_user_dataset_from_ids(column_list, dataset_ids)
+    context = {
+        "dataset_ids": [],
+        "data": [],
+        "error": None,
+        "downloadable": None
+    }
+    
+    if request.method == "GET":
+        return render()
+    
+    form = Dataset_operation_form(request)
+    dataset_ids = form.get_ids_as_list()
+    
+    if not dataset_ids:
+        return render(request, "", context = context)
+    
+    if not form.is_valid():
+        context["error"] = "Unsupported dataset combination"
+        context["dataset_ids"] = dataset_ids
+    
+        return render(request, "", context = context)
+    
+    column_list = ["su.user_full_name", "s.display_name"]
+    user_data = sub_select_user_dataset_from_ids(column_list, dataset_ids)
 
-
+    
 
 
     G = nx.complete_bipartite_graph(3, 3)
     fig, ax = plt.subplots(1, 1)
     nx.draw_networkx(G, ax=ax)
     html_embed_string = fig_to_html(fig)
-    
-    s = sub_select_comment_dataset_from_ids(["sc.text_body"], ["prsc_55"])
-    print(s)
 
     return render(request, "graphs/base_graph.html", {"html_embed": html_embed_string})
