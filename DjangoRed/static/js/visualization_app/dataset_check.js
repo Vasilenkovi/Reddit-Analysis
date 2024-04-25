d45bb1ff382ae7325a7861e0f71eec999a73d8f3
Ssssssaber <bladimir.lobanov@gmail.com>
2024-04-26 04:29:45 +0700

    (RED-95) Update: updated main styles for all pages

--- DjangoRed/static/js/visualization_app/dataset_check.js
+++ DjangoRed/static/js/visualization_app/dataset_check.js
@@ -1,12 +1,21 @@
+var form_label
+var datasetIds
 function add_dataset_button(e) {
     const dataset_container = document.getElementById("dataset-holder")
 
     const add_dataset_input = document.getElementById("add-dataset-source")
     const dataset_id = add_dataset_input.value
+
+    
     
     if (dataset_id.substring(0, 5) != "prsc_") {
         
         alert("Wrong dataset id!")
+        return
+    }
+    else if (isAlreadyAdded(dataset_id))
+    {
+        alert("Dataset already added!")
         return
     }
 
@@ -26,13 +35,37 @@ function add_dataset_button(e) {
         label_tag.innerHTML = dataset_id
 
     div_container.appendChild(label_tag)
-
+    
     dataset_container.appendChild(div_container)
+
+    check_if_added()
 }
+
+function check_if_added() {
+    let datasetsIds = document.getElementsByClassName("dataset_id")
+    if (datasetsIds.length != 0) {
+        form_label.innerHTML = "Selected datasets:" 
+    }
+}
+
+function isAlreadyAdded(idToCheck) {
+    let datasetsIds = document.getElementsByClassName("dataset_id")
+    if (datasetsIds == null) return false;
+
+    for (var dataset_id of datasetsIds) {
+        if (dataset_id.name == idToCheck) {
+            return true;
+        }
+    }
+    return false;
+}
+   
+
 
 function main() {
     const add_button = document.getElementById("add-dataset")
-
+    form_label = document.getElementById("form-header")
+    check_if_added()
     add_button.addEventListener("click", add_dataset_button)
 }
 
