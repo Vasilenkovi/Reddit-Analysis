532b53f2edf7981bd5ff7922bc3c95c1c2acc7d6
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2024-05-06 01:19:15 +0700

    Return

--- MySQL/parser_database/usersPars.sql
+++ MySQL/parser_database/usersPars.sql
@@ -1,11 +1,11 @@
-CREATE USER 'reddit_client'@'%' IDENTIFIED BY 'Reddit_Yarik_1984!';
-GRANT SELECT, INSERT, UPDATE, DELETE ON reddit_parsing.* TO 'reddit_client'@'%';
+CREATE USER 'reddit_client'@'localhost' IDENTIFIED BY 'Reddit_Yarik_1984!';
+GRANT SELECT, INSERT, UPDATE, DELETE ON reddit_parsing.* TO 'reddit_client'@'localhost';
 FLUSH PRIVILEGES;
 
-CREATE USER 'parser_read'@'%' IDENTIFIED BY 'bgqld9(#_5]Y]86';
-GRANT SELECT ON reddit_parsing.* TO 'parser_read'@'%';
+CREATE USER 'parser_read'@'localhost' IDENTIFIED BY 'bgqld9(#_5]Y]86';
+GRANT SELECT ON reddit_parsing.* TO 'parser_read'@'localhost';
 FLUSH PRIVILEGES;
 
-CREATE USER 'clusterizer'@'%' IDENTIFIED BY 'Clusterizing1984!';
-GRANT SELECT on reddit_parsing.* to 'clusterizer'@'%';
+CREATE USER 'clusterizer'@'localhost' IDENTIFIED BY 'Clusterizing1984!';
+GRANT SELECT on reddit_parsing.* to 'clusterizer'@'localhost';
 FLUSH PRIVILEGES;
