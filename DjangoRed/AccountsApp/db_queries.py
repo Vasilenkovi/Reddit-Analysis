150111d8ba416686ef0ca69f9654787663d73fd9
Ssssssaber <bladimir.lobanov@gmail.com>
2024-05-20 01:07:16 +0700

    update queries

--- DjangoRed/AccountsApp/db_queries.py
+++ DjangoRed/AccountsApp/db_queries.py
@@ -4,22 +4,55 @@ from mysql.connector import Connect
 from json import loads
 
 
-# def get_user_favorite_jog_ids(limit: int = 100, offset: int = 0) -> list[tuple]:
-#     query = """SELECT job_id from job_id where job_id = job_id"""
+def __mysql_query(job_type: Job_types, table_name: Valid_tables, in_query: dict) -> str:
+    if table_name not in vars(Valid_tables).keys():
+        raise AttributeError("Unsupported table name")
 
-#     params = {
-#         "offset": offset,
-#         "limit": limit
-#     }
-
-def execute(database_dict: dict, query: str, params: dict) -> list[tuple]:
-
-    cnx = Connect(**database_dict)
+    cnx = Connect(**NATIVE_SQL_DATABASES['job_id'])
     cur = cnx.cursor()
     cur.reset()
 
-    cur.execute(query, params = params)
-    r = cur.fetchall()
+    query = """INSERT INTO reddit_job_id.{safe_table_name}(query, created_timestamp) 
+            VALUES (%(query)s, FROM_UNIXTIME(%(timestamp)s))""".format(safe_table_name = table_name)
+    cur.execute(query, params = {
+        'query': dumps(in_query),
+        'timestamp': int(time.time())
+    })
+    cur.reset()
+
+    query = """SELECT LAST_INSERT_ID()"""
+    cur.execute(query)
+    psysical_id = cur.fetchall()[0][0]
+    cur.reset()
+
+    new_task_id = job_type + "_" + str(psysical_id)
+
+    query = """UPDATE reddit_job_id.{safe_table_name} 
+            SET task_id = %(new_task_id)s
+            WHERE id = %(new_id)s""".format(safe_table_name = table_name)
+    cur.execute(query, params = {
+        'new_task_id': new_task_id,
+        'new_id': psysical_id
+    })
+    cur.reset()
+
+    cnx.commit()
     cnx.close()
+
+    return new_task_id
+
+def chekc_for_exsisting_datasets():
+    cnx = Connect(**NATIVE_SQL_DATABASES['job_id'])
+
+    query = """SELECT FROM reddit_job_id.{safe_table_name}(query, created_timestamp) 
+            VALUES (%(query)s, FROM_UNIXTIME(%(timestamp)s))""".format(safe_table_name = table_name)
+
+    # cnx = Connect(**database_dict)
+    # cur = cnx.cursor()
+    # cur.reset()
+
+    # cur.execute(query, params = params)
+    # r = cur.fetchall()
+    # cnx.close()
 
     return r
