49481835cae95f06841036df3de4a1d2f8fa0115
Ssssssaber <bladimir.lobanov@gmail.com>
2024-05-20 03:15:28 +0700

    (RED-117) Update: added check for up to date job ids

--- DjangoRed/AccountsApp/db_queries.py
+++ DjangoRed/AccountsApp/db_queries.py
@@ -4,55 +4,24 @@ from mysql.connector import Connect
 from json import loads
 
 
-def __mysql_query(job_type: Job_types, table_name: Valid_tables, in_query: dict) -> str:
-    if table_name not in vars(Valid_tables).keys():
-        raise AttributeError("Unsupported table name")
-
+def check_for_up_to_date_job_ids():
     cnx = Connect(**NATIVE_SQL_DATABASES['job_id'])
+    
+    job_ids = []
+    queries = [
+        f"""SELECT task_id FROM reddit_job_id.parsing_comment_id;""",
+        f"""SELECT task_id FROM reddit_job_id.clustering_id;""",
+        f"""SELECT task_id FROM reddit_job_id.parsing_subreddits_id;"""
+    ]
+    
     cur = cnx.cursor()
-    cur.reset()
-
-    query = """INSERT INTO reddit_job_id.{safe_table_name}(query, created_timestamp) 
-            VALUES (%(query)s, FROM_UNIXTIME(%(timestamp)s))""".format(safe_table_name = table_name)
-    cur.execute(query, params = {
-        'query': dumps(in_query),
-        'timestamp': int(time.time())
-    })
-    cur.reset()
-
-    query = """SELECT LAST_INSERT_ID()"""
-    cur.execute(query)
-    psysical_id = cur.fetchall()[0][0]
-    cur.reset()
-
-    new_task_id = job_type + "_" + str(psysical_id)
-
-    query = """UPDATE reddit_job_id.{safe_table_name} 
-            SET task_id = %(new_task_id)s
-            WHERE id = %(new_id)s""".format(safe_table_name = table_name)
-    cur.execute(query, params = {
-        'new_task_id': new_task_id,
-        'new_id': psysical_id
-    })
-    cur.reset()
-
-    cnx.commit()
+    for query in queries:
+        cur.reset()        
+        cur.execute(query)
+        r = cur.fetchall()
+        for tup in r:
+            job_ids.append(tup[0])
+    
     cnx.close()
 
-    return new_task_id
-
-def chekc_for_exsisting_datasets():
-    cnx = Connect(**NATIVE_SQL_DATABASES['job_id'])
-
-    query = """SELECT FROM reddit_job_id.{safe_table_name}(query, created_timestamp) 
-            VALUES (%(query)s, FROM_UNIXTIME(%(timestamp)s))""".format(safe_table_name = table_name)
-
-    # cnx = Connect(**database_dict)
-    # cur = cnx.cursor()
-    # cur.reset()
-
-    # cur.execute(query, params = params)
-    # r = cur.fetchall()
-    # cnx.close()
-
-    return r
+    return job_ids
