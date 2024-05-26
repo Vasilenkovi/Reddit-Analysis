55af1129f06b86a7dc95352b8b2ad66f38c79c38
Kshimir DELQ <dragonworld15@gmail.com>
2024-05-27 02:13:27 +0700

    Fixed .cs mistake Made Fix in consumer.py (picture overwrite)

--- DjangoRed/StatApp/consumer.py
+++ DjangoRed/StatApp/consumer.py
@@ -20,11 +20,12 @@ class StatConsumer(WebsocketConsumer):
     def receive(self, text_data=None, bytes_data=None):
         received_message = json.loads(text_data)
         submissonNames = received_message["names"]
+        work_job_id = received_message["job_id"]
         #try:
 
-        f_query = """ SELECT st.word_cloud, st.full_name, st.upvote
+        f_query = """ SELECT st.word_cloud, st.full_name, st.upvote, st.job_id
                   FROM statisticsdb.statistic AS st
-                  WHERE st.full_name {in_expr}
+                  WHERE st.full_name {in_expr} AND st.job_id = '{work_job_id}'
                   ORDER BY st.upvote
                   DESC; """
 
@@ -35,19 +36,19 @@ class StatConsumer(WebsocketConsumer):
             params[s] = s
             wrapped.append(f"%({s})s")
 
-        in_expr += ", ".join(wrapped) +" )"
+        in_expr += ", ".join(wrapped) + " )"
 
-        query = f_query.format(in_expr = in_expr)
+        query = f_query.format(in_expr = in_expr, work_job_id = work_job_id)
         print(query)
 
         table_data = execute(NATIVE_SQL_DATABASES['stat'], query, params)
-        test_message_back = []
+        message_back = []
         for i in table_data:
-            test_message_back.append(str(i[0])[2:-1])
+            message_back.append(str(i[0])[2:-1])
         
         self.send(text_data=json.dumps({
             "type": "info",
-            "info": test_message_back,
+            "info": message_back,
             }))
 
     def disconnect(self, close_code):
