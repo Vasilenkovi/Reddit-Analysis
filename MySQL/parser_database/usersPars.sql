bf0c32ed93b64c7fe92426da5409b68ce8526ec0
CatGamer7 <arthur.zayda@gmail.com>
2024-05-30 04:10:43 +0700

    (RED-121) update: configs

--- MySQL/parser_database/usersPars.sql
+++ MySQL/parser_database/usersPars.sql
@@ -1,4 +1,4 @@
-CREATE USER 'reddit_client'@'%' IDENTIFIED BY 'Reddit_Yarik_1984!';
+CREATE USER 'reddit_client'@'%' IDENTIFIED BY 'f2O$$a6rA39Jcq]Y';
 GRANT SELECT, INSERT, UPDATE, DELETE ON reddit_parsing.* TO 'reddit_client'@'%';
 FLUSH PRIVILEGES;
 
@@ -6,6 +6,6 @@ CREATE USER 'parser_read'@'%' IDENTIFIED BY 'bgqld9(#_5]Y]86';
 GRANT SELECT ON reddit_parsing.* TO 'parser_read'@'%';
 FLUSH PRIVILEGES;
 
-CREATE USER 'clusterizer'@'%' IDENTIFIED BY 'Clusterizing1984!';
+CREATE USER 'clusterizer'@'%' IDENTIFIED BY '5AP@32%/VtDo_J+9';
 GRANT SELECT on reddit_parsing.* to 'clusterizer'@'%';
 FLUSH PRIVILEGES;
