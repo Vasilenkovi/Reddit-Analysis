0c6610e5d3c75d28e2380366e3939ac69f552d86
ShizVizor <46966177+Vasilenkovi@users.noreply.github.com>
2024-05-31 00:31:05 +0700

    Update Optics.py

--- DjangoRed/VisualizationApp/services/Optics.py
+++ DjangoRed/VisualizationApp/services/Optics.py
@@ -3,10 +3,12 @@ import numpy as np
 class Optics:
     def __init__(self,doc_to_doc_matrix):
         self.doc_to_doc_matrix = doc_to_doc_matrix
+        self.result = None
     def cluster(self):
         X = np.array(self.doc_to_doc_matrix)
         clustering = OPTICS().fit(X)
         self.save_to_db()
+        self.result = clustering.labels_
         return clustering.labels_
     def save_to_db(self):
         pass
