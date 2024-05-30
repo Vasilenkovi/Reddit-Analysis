b64b7e440a9e8b6b30e973ab692254aaa96aa758
CatGamer7 <99312965+CatGamer7@users.noreply.github.com>
2024-05-30 19:34:17 +0700

    Update usersAccounts.sql

--- MySQL/user_database/usersAccounts.sql
+++ MySQL/user_database/usersAccounts.sql
@@ -3,5 +3,5 @@ GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER ON reddit_test_accounts.* TO
 FLUSH PRIVILEGES;
 
 CREATE USER 'test_accounts_client'@'%' IDENTIFIED BY 't9a!4Ic1G+X';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER ON reddit_test_accounts.* TO 'test_accounts_client'@'%';
+GRANT SELECT, REFERENCES, INSERT, UPDATE, DELETE, CREATE, ALTER ON reddit_test_accounts.* TO 'test_accounts_client'@'%';
 FLUSH PRIVILEGES;
